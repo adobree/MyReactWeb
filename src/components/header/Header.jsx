658a4c5d38b316738ed2b9ab98eccc708d7658f4
adobree <ado.kovacs@gmail.com>
2022-05-22 09:42:10 +0200

    first push

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
+import ME from '../../assets/me.png'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Adam Kovacs</h1>
+        <h5 className="text-light">Fullstack developer</h5>
+        <CTA />
+
+        <div className="me">
+          <img src={ME} alt="me" />
+        </div>
+
+        <a href="#contact" className='scroll__down'>Scroll Down</a>
+      </div>
+    </header>
   )
 }
 
---
+++ src/components/header/HeaderSocials.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const HeaderSocials = () => {
+  return (
+    <div className="header__socials">
+        <a href="http://" target="_blank"></a>
+        <a href="http://" target="_blank"></a>
+        <a href="http://" target="_blank"></a>
+    </div>
+  )
+}
+
+export default HeaderSocials
