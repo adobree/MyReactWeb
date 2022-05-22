4127ed6feb29366816a312e8c0e9ee9119f68832
Adam Kovacs <kovacs.adam@itexpress.hu>
2022-05-22 19:38:24 +0200

    done services

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,9 +1,76 @@
 import React from 'react'
 import './experience.css'
+import {BsFillPatchCheckFill} from 'react-icons/bs'
 
 const Experience = () => {
   return (
-    <section id='experience'>Experience</section>
+    <section id='experience'>
+      <h5>What Skills I Have</h5>
+      <h2>My Experience</h2>
+
+      <div className="container experience__container">
+        <div className="experience__frontend">
+          <h3>Frontend Developer</h3>
+          <div className="experience__content">
+            <article className='experience__details'>
+              <BsFillPatchCheckFill className='experience__details-icon'/>
+              <div><h4>HTML</h4>
+              <small className="text-light">Experienced</small></div>
+            </article>
+            <article className='experience__details'>
+              <BsFillPatchCheckFill className='experience__details-icon'/>
+              <div><h4>CSS</h4>
+              <small className="text-light">Experienced</small></div>
+            </article>
+            <article className='experience__details'>
+              <BsFillPatchCheckFill className='experience__details-icon'/>
+              <div><h4>JS</h4>
+              <small className="text-light">Experienced</small></div>
+            </article>
+            <article className='experience__details'>
+              <BsFillPatchCheckFill className='experience__details-icon'/>
+              <div><h4>Bootstrap</h4>
+              <small className="text-light">Experienced</small></div>
+            </article>
+            <article className='experience__details'>
+              <BsFillPatchCheckFill className='experience__details-icon'/>
+              <div><h4>React</h4>
+              <small className="text-light">Experienced</small></div>
+            </article>
+          </div>
+        </div>
+        <div className="experience__backend">
+        <h3>Backend Developer</h3>
+          <div className="experience__content">
+            <article className='experience__details'>
+              <BsFillPatchCheckFill className='experience__details-icon'/>
+              <div><h4>PHP</h4>
+              <small className="text-light">Experienced</small></div>
+            </article>
+            <article className='experience__details'>
+              <BsFillPatchCheckFill className='experience__details-icon'/>
+              <div><h4>Node JS</h4>
+              <small className="text-light">Experienced</small></div>
+            </article>
+            <article className='experience__details'>
+              <BsFillPatchCheckFill className='experience__details-icon'/>
+              <div><h4>MongoDB</h4>
+              <small className="text-light">Experienced</small></div>
+            </article>
+            <article className='experience__details'>
+              <BsFillPatchCheckFill className='experience__details-icon'/>
+              <div><h4>MySQL</h4>
+              <small className="text-light">Experienced</small></div>
+            </article>
+            <article className='experience__details'>
+              <BsFillPatchCheckFill className='experience__details-icon'/>
+              <div><h4>React</h4>
+              <small className="text-light">Experienced</small></div>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,7 +3,20 @@ import './portfolio.css'
 
 const Portfolio = () => {
   return (
-    <section id='portfolio'>Portfolio</section>
+    <section id='portfolio'>
+      <h5>My Portfolio</h5>
+      <h2>Portfolio</h2>
+
+      <div className="container portfolio__container">
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <h3>This is a portfolio item</h3>
+            <a href="#" className='btn'>GitHub</a>
+            <a href="#" className='btn btn-primary'>GitHub</a>
+          </div>
+        </article>
+      </div>
+    </section>
   )
 }
 
